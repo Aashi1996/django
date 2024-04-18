94985bd51d47d8742a9ff776a648fe2a9069b889
aashi <aashijain.sangamone@gmail.com>
2024-04-18 06:32:51 +0530

    'holiday1'

--- P1/Sunday/views.py
+++
@@ -1,9 +0,0 @@
-import datetime
-from django.shortcuts import render
-
-# Create your views here.
-def index(request):
-    today = datetime.datetime.today()
-    return render(request, "Sunday/index.html", {
-        "Sunday": today.weekday() == 6
-        })
